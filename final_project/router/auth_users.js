449c8389dc60c0c7489060bbc248779d876415be
German Heredia <germanheredia121@gmail.com>
2026-07-15 20:51:32 -0300

    bookstore

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -6,23 +6,54 @@ const regd_users = express.Router();
 let users = [];
 
 const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+  return users.some((user) => user.username === username);
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
-}
+  return users.some((user) => user.username === username && user.password === password)
+};
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const { username, password } = req.body;
+
+  if (!username || !password){
+    return res.status(400).json({ message : "Username and password are required"});
+  }
+  if (!isValid(username)){
+    return res.status(401).json({ message: "User does not exist"});
+  }
+  if (!authenticatedUser(username, password)){
+    return res.status(401).json({ message : "Invalid password"})
+  }
+
+  let accessToken = jwt.sign(
+    {data: username},
+    "access",
+    {expiresIn: 60 * 60}
+  );
+
+  req.session.authorization = {
+    accessToken,
+    username
+  };
+  return res.status(200).json({ message : "Login successful"})
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  const review = req.body.review;
+
+  if(!review){
+    return res.status(400).json({message: "Review is required"});
+  }
+
+  if(!books[isbn]){
+    return res.status(404).json({message: "Book not found"});
+  }
+  books[isbn].reviews[review] = review;
+  return res.status(200).json({message: "Review added successfully"})
 });
 
 module.exports.authenticated = regd_users;
